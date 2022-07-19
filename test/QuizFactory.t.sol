bc4141a2e642813cbaffad9c6c3d8e935814e66e
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-07-19 08:46:02 -0400

    add createQuiz function to contract and test

--- test/QuizFactory.t.sol
+++ test/QuizFactory.t.sol
@@ -11,4 +11,19 @@ contract QuizFactoryTest is Test {
     function setUp() public {
         factory = new QuizFactory();
     }
+
+    function testCreateQuiz() public {
+        string memory question = "What is the answer to life?";
+        string memory answer = "42";
+        bytes32 salt = bytes32("123123123");
+        bytes32 hashedAnswer = keccak256(abi.encodePacked(salt, answer));
+        emit log_bytes32(hashedAnswer);
+
+        factory.createQuiz(question, hashedAnswer);
+        QuizGame quiz = factory.quizzes(0);
+        assertEq(
+            keccak256(abi.encodePacked(quiz.question())),
+            keccak256(abi.encodePacked(question))
+        );
+    }
 }
